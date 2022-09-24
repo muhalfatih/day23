86cde90c4f259ad448e4d0cd20ef80aa82f1b607
muhalfatih <muhammadal.fatih9@gmail.com>
2022-09-24 07:48:32 +0700

    add users

---
+++ users/app.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+const users = {
+    1: {
+        fullname: "Zaky Zaidan",
+        username: "zakynich",
+        address: "Jakarta"
+    },
+    2: {
+        fullname: "M. Fathur Ahmad",
+        username: "fathurtuch",
+        address: "Samarinda"
+    },
+    3: {
+        fullname: "Sumanto Karomahi",
+        username: "sukar",
+        address: "Bandung"
+    }
+}
+
+app.get('/user', function(req, res){
+    let body = req.body
+    console.log(body)
+
+    if(Object.keys(body).length !== 0){
+        let lastNum = Object.keys(users).pop()
+        users[++lastNum] = body
+    }
+
+    res.send(users)
+})
+
+app.get('/user/:id', function(req, res){
+    res.send(users[req.params.id])
+})
+
+console.log(`Listening Port: ${port}`)
+
+app.listen(port)
